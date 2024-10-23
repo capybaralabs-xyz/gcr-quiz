8671bb0394ccb0048c32c45fa88ff732787e511a
jerry <jerry@capybaralabs.xyz>
2024-10-23 12:49:58 +0800

    update: revoke replace url

--- src/app/result/[character]/page.tsx
+++ src/app/result/[character]/page.tsx
@@ -29,7 +29,7 @@ function ResultPage({
       if (!response.ok) {
         console.error('Failed to send data to /api/character')
       } else {
-        // window.location.replace('https://ladder.top')
+        window.location.replace('https://ladder.top')
       }
     } catch (error) {
       console.error('Error:', error)
